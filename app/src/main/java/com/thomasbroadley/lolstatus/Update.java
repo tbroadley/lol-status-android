40da34d91dba8c52047ab16498e97bd96b5b67ea
Thomas Broadley <htbroadl@uwaterloo.ca>
2014-09-17 13:23:30 -0400

    Fixed time zone for status update times

--- app/src/main/java/com/thomasbroadley/lolstatus/Update.java
+++ app/src/main/java/com/thomasbroadley/lolstatus/Update.java
@@ -4,6 +4,7 @@ import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 /**
 * Created by Thomas on 2014-09-13.
@@ -14,7 +15,10 @@ public class Update {
 
     public Update(JSONObject json) throws Exception{
         message = json.getString("content");
-        updated = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss.SSS'Z'").parse(json.getString("updated_at"));
+
+        SimpleDateFormat sdf =  new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss.SSS'Z'");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+        updated = sdf.parse(json.getString("created_at"));
     }
 
     public String getMessage() {
