5dd5f150fd60f664015820524c08ebf5a52e223d
Thomas Broadley <htbroadl@uwaterloo.ca>
2014-09-20 16:37:40 -0400

    Cleaned up unnecessary code in MainPage.java

--- app/src/main/java/com/thomasbroadley/lolstatus/MainPage.java
+++ app/src/main/java/com/thomasbroadley/lolstatus/MainPage.java
@@ -11,20 +11,10 @@ import java.util.ArrayList;
 
 public class MainPage extends ActionBarActivity {
 
-    final String URL = "http://status.leagueoflegends.com/shards/";
-
-    final String[] SERVER = new String[] {"br", "eune", "euw", "lan", "las", "na", "oce", "ru", "tr"};
-    ArrayList<String> serverName;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
-
-        serverName = new ArrayList<String>();
-
-        ConnectivityManager manager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo info = manager.getActiveNetworkInfo();
 
         MainPageFragment mainPage = new MainPageFragment();
 
