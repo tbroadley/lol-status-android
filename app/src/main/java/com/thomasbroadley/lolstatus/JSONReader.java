ccf3cbd951e26f05c4c771b796afd9019cdc1837
Thomas Broadley <htbroadl@uwaterloo.ca>
2014-09-29 17:55:01 -0400

    Now using HttpURLConnection to update status

--- app/src/main/java/com/thomasbroadley/lolstatus/JSONReader.java
+++ app/src/main/java/com/thomasbroadley/lolstatus/JSONReader.java
@@ -2,16 +2,15 @@ package com.thomasbroadley.lolstatus;
 
 import android.os.AsyncTask;
 
+import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
+import java.io.BufferedInputStream;
 import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class JSONReader {
-    String json;
-
     public JSONReader() {}
 
     public JSONObject read(String url) throws Exception {
@@ -24,24 +23,19 @@ public class JSONReader {
 
         @Override
         protected String doInBackground(String... url) {
-            StringBuilder json = new StringBuilder();
-            InputStream is;
+            String json = "";
 
             try {
-                is = new URL(url[0]).openStream();
-                BufferedReader rd = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-                String line;
-                json = new StringBuilder();
-
-                while ((line = rd.readLine()) != null) {
-                    json.append(line);
-                }
-                is.close();
+                URL site = new URL(url[0]);
+                HttpURLConnection connection = (HttpURLConnection)site.openConnection();
+                InputStream in = new BufferedInputStream(connection.getInputStream());
+                json = IOUtils.toString(in, "UTF-8");
+                connection.disconnect();
             } catch (Exception e) {
                 e.printStackTrace();
             }
 
-            return json.toString();
+            return json;
         }
     }
 }
